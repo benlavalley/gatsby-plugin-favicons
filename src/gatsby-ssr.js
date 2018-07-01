6e3678af730e8f3f5729092c99821a9dc0dc39a2
Mrtenz <maarten@zuidhoorn.com>
2018-07-01 11:28:19 +0200

    Fix path prefix in development mode

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -14,11 +14,12 @@ module.exports.onRenderBody = function(
     }
   }
 ) {
+  const prefix = pathPrefix || '/';
   const components = [];
 
   if (android) {
     components.push(
-      <link key="android-manifest" rel="manifest" href={`${pathPrefix}favicons/manifest.json`} />
+      <link key="android-manifest" rel="manifest" href={`${prefix}favicons/manifest.json`} />
     );
   }
 
@@ -28,55 +29,55 @@ module.exports.onRenderBody = function(
         key="apple-touch-icon-57x57"
         rel="apple-touch-icon"
         sizes="57x57"
-        href={`${pathPrefix}favicons/apple-touch-icon-57x57.png`}
+        href={`${prefix}favicons/apple-touch-icon-57x57.png`}
       />,
       <link
         key="apple-touch-icon-60x60"
         rel="apple-touch-icon"
         sizes="60x60"
-        href={`${pathPrefix}favicons/apple-touch-icon-60x60.png`}
+        href={`${prefix}favicons/apple-touch-icon-60x60.png`}
       />,
       <link
         key="apple-touch-icon-72x72"
         rel="apple-touch-icon"
         sizes="72x72"
-        href={`${pathPrefix}favicons/apple-touch-icon-72x72.png`}
+        href={`${prefix}favicons/apple-touch-icon-72x72.png`}
       />,
       <link
         key="apple-touch-icon-76x76"
         rel="apple-touch-icon"
         sizes="76x76"
-        href={`${pathPrefix}favicons/apple-touch-icon-76x76.png`}
+        href={`${prefix}favicons/apple-touch-icon-76x76.png`}
       />,
       <link
         key="apple-touch-icon-114x114"
         rel="apple-touch-icon"
         sizes="114x114"
-        href={`${pathPrefix}favicons/apple-touch-icon-114x114.png`}
+        href={`${prefix}favicons/apple-touch-icon-114x114.png`}
       />,
       <link
         key="apple-touch-icon-120x120"
         rel="apple-touch-icon"
         sizes="120x120"
-        href={`${pathPrefix}favicons/apple-touch-icon-120x120.png`}
+        href={`${prefix}favicons/apple-touch-icon-120x120.png`}
       />,
       <link
         key="apple-touch-icon-144x144"
         rel="apple-touch-icon"
         sizes="144x144"
-        href={`${pathPrefix}favicons/apple-touch-icon-144x144.png`}
+        href={`${prefix}favicons/apple-touch-icon-144x144.png`}
       />,
       <link
         key="apple-touch-icon-152x152"
         rel="apple-touch-icon"
         sizes="152x152"
-        href={`${pathPrefix}favicons/apple-touch-icon-152x152.png`}
+        href={`${prefix}favicons/apple-touch-icon-152x152.png`}
       />,
       <link
         key="apple-touch-icon-180x180"
         rel="apple-touch-icon"
         sizes="180x180"
-        href={`${pathPrefix}favicons/apple-touch-icon-180x180.png`}
+        href={`${prefix}favicons/apple-touch-icon-180x180.png`}
       />
     );
   }
@@ -87,61 +88,61 @@ module.exports.onRenderBody = function(
         key="apple-touch-startup-image-320x460"
         rel="apple-touch-startup-image"
         media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-320x460.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-320x460.png`}
       />,
       <link
         key="apple-touch-startup-image-460x920"
         rel="apple-touch-startup-image"
         media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-640x920.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-640x920.png`}
       />,
       <link
         key="apple-touch-startup-image-640x1096"
         rel="apple-touch-startup-image"
         media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-640x1096.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-640x1096.png`}
       />,
       <link
         key="apple-touch-startup-image-750x1294"
         rel="apple-touch-startup-image"
         media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-750x1294.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-750x1294.png`}
       />,
       <link
         key="apple-touch-startup-image-1182x2208"
         rel="apple-touch-startup-image"
         media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-1182x2208.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-1182x2208.png`}
       />,
       <link
         key="apple-touch-startup-image-1242x2148"
         rel="apple-touch-startup-image"
         media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-1242x2148.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-1242x2148.png`}
       />,
       <link
         key="apple-touch-startup-image-748x1024"
         rel="apple-touch-startup-image"
         media="(device-width: 748px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: landscape)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-748x1024.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-748x1024.png`}
       />,
       <link
         key="apple-touch-startup-image-768x1004"
         rel="apple-touch-startup-image"
         media="(device-width: 768px) and (device-height: 1004px) and (-webkit-device-pixel-ratio: 1) and (orientation: portrait)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-768x1004.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-768x1004.png`}
       />,
       <link
         key="apple-touch-startup-image-1496x2048"
         rel="apple-touch-startup-image"
         media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-1496x2048.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-1496x2048.png`}
       />,
       <link
         key="apple-touch-startup-image-1536x2008"
         rel="apple-touch-startup-image"
         media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        href={`${pathPrefix}favicons/apple-touch-startup-image-1536x2008.png`}
+        href={`${prefix}favicons/apple-touch-startup-image-1536x2008.png`}
       />
     );
   }
@@ -152,7 +153,7 @@ module.exports.onRenderBody = function(
         key="coast-228x228"
         rel="icon"
         sizes="228x228"
-        href={`${pathPrefix}favicons/coast-228x228.png`}
+        href={`${prefix}favicons/coast-228x228.png`}
       />
     );
   }
@@ -163,15 +164,15 @@ module.exports.onRenderBody = function(
         key="favicon-32x32"
         rel="icon"
         sizes="32x32"
-        href={`${pathPrefix}favicons/favicon-32x32.png`}
+        href={`${prefix}favicons/favicon-32x32.png`}
       />,
       <link
         key="favicon-16x16"
         rel="icon"
         sizes="16x16"
-        href={`${pathPrefix}favicons/favicon-16x16.png`}
+        href={`${prefix}favicons/favicon-16x16.png`}
       />,
-      <link key="favicon" rel="shortcut icon" href={`${pathPrefix}favicons/favicon.ico`} />
+      <link key="favicon" rel="shortcut icon" href={`${prefix}favicons/favicon.ico`} />
     );
   }
 
@@ -180,7 +181,7 @@ module.exports.onRenderBody = function(
       <link
         key="yandex-browser-manifest"
         rel="yandex-tableau-widget"
-        href={`${pathPrefix}favicons/yandex-browser-manifest.json`}
+        href={`${prefix}favicons/yandex-browser-manifest.json`}
       />
     );
   }
@@ -190,12 +191,12 @@ module.exports.onRenderBody = function(
       <meta
         key="mstile-144x144"
         name="msapplication-TileImage"
-        content={`${pathPrefix}favicons/mstile-144x144.png`}
+        content={`${prefix}favicons/mstile-144x144.png`}
       />,
       <meta
         key="browserconfig"
         name="msapplication-config"
-        content={`${pathPrefix}favicons/browserconfig.xml`}
+        content={`${prefix}favicons/browserconfig.xml`}
       />
     );
   }
